35b7ac87937e92e8970f17042d65ffdbeaef9e04
Shanice Dunk <shanice.dunk@icloud.com>
2024-07-04 20:32:28 +0100

    Updating object oriented

--- exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/shapes/Paint.java
+++ exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/shapes/Paint.java
@@ -1,17 +1,15 @@
 package com.cbfacademy.shapes;
 
 public class Paint {
-    protected double gallon;
-    protected double squareFeet;
     protected double coverage;
-    protected double paint;
+   public Paint(double coverage){
+    this.coverage=coverage;
+   }
+   //Method to calculate the amount of paint needed
+   
+   public double getAmount(Shape shape){
+    double area =shape.getArea();
+    return area/coverage;
+   }
     
-
-    public Paint(double coverage) {
-       this.coverage = coverage;
-    }
-
-    public void getAmount(Shape shape) {
-        
-    }
 }
