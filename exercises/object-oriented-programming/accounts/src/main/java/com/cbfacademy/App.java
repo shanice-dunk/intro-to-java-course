354aa9f8ad311970aa3ab415cf6f7a80f33fd2b3
Shanice Dunk <shanice.dunk@icloud.com>
2024-07-27 20:15:04 +0100

    OOP stretch exercise

--- exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/App.java
+++ exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/App.java
@@ -1,13 +1,41 @@
 package com.cbfacademy;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cbfacademy.accounts.Account;
+import com.cbfacademy.accounts.CurrentAccount;
+import com.cbfacademy.accounts.SavingsAccount;
+
 public class App {
+    // Create a list to store accounts
+    private static List<Account> accountsList = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+                // Add instances of each class to the list
+                accountsList.add(new Account(001, 1000.0));
+                accountsList.add(new SavingsAccount(002, 2000.0, 1.5));
+                accountsList.add(new CurrentAccount(003, 3000.0, 500.0));
+        
+                // Print the accounts in the list
+                for (Account account : accountsList) {
+                    System.out.println(account);
+                }
+            }
+        private static void applyInterestToSavingsAccounts() {
+            for (Account account : accountsList) {
+                if (account instanceof SavingsAccount) {
+                    ((SavingsAccount) account).applyInterest();
+                    System.out.println("Interest applied to: " + account);
+                }
+            }
+        }
+        }
+
+
+         // System.out.println("Hello World!");
         // Showroom car = new Showroom();
         // getCars();
 
         // for (Car car : cars) {
         //     System.out.println(car.getCars());
         // }
-    }
-}
